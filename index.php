2839d96f32097b6fc597697b8e79f45bb81c1606
FuglyShmugly <kornsick@gmail.com>
2026-08-17 09:15:02 +0300

    fixed

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gamelendar</title>
+    </head>
+<body>
 <style>
 html,
 body {
@@ -411,3 +419,7 @@ echo '</div>';
 
 // Usage
 showNotableReleases();
+?>
+
+</body>
+</html>
